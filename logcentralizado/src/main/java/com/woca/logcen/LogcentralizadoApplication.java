e64ff41919e488e1231d963fb58f75f4ed243648
Walbety Castro <walbety.castro.araujo@everis.com>
2020-05-27 13:07:22 -0300

    adicionado projeto e pasta com docker-compose

---
+++ logcentralizado/src/main/java/com/woca/logcen/LogcentralizadoApplication.java
@@ -0,0 +1,13 @@
+package com.woca.logcen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogcentralizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogcentralizadoApplication.class, args);
+	}
+
+}
---
+++ logcentralizado/src/test/java/com/woca/logcen/LogcentralizadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woca.logcen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogcentralizadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
